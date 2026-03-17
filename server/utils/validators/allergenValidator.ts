5ae24636635c920cf0c3d8c6c7ab33171135c705
Laura Hofmanova <N00222003@iadt.ie>
2026-03-17 23:04:20 +0000

    added in validators

---
+++ server/utils/validators/allergenValidator.ts
@@ -0,0 +1,32 @@
+import {z} from "zod";
+
+export const allergenSchema = z.object ({
+    name: z
+        .string()
+        .trim()
+        .min(2, "Allergen must be at least 2 characters")
+        .max(100, "Allergen name must be at most 100 characters"),
+    description: z
+        .string()
+        .trim()
+        .min(2, "Allergen description must be at    least 2 characters")
+        .max(500, "Allergen description must be at most 500 characters"),
+})
+
+export const updateAllergenSchema= z.object({
+    name: z
+        .string()
+        .trim()
+        .min(2, "Allergen name must be at least 2 characters")
+        .max(100, "Allergen name must be at most 100 characters")
+	    .optional(),
+    description: z
+			.string()
+			.trim()
+			.min(2, "Condition description must be at least 2 characters")
+			.max(500, "Condition description must be at most 500 characters")
+			.optional(),
+})
+.refine((data)=>Object.keys(data).length > 0,{
+    message: "At least one field must be provided for update",
+})
---
+++ server/utils/validators/preferenceValidator.ts
@@ -0,0 +1,30 @@
+import {z} from "zod";
+
+export const preferenceSchema = z.object({
+    name:z
+        .string()
+        .trim()
+        .min(2,"Preference name must be at least 2 characters")
+        .max (100,"Preference name must be at most 100 characters"),
+    description: z
+        .string()
+        .trim()
+        .min(2,"Allergen description must be at least 2 characters")
+        .max(100,"Allergen description must be at most 500 characters" )
+})
+
+export const updateAllergenSchema = z.object({
+    name:z
+        .string()
+        .trim()
+        .min(2, "Preference name must be at least 2 characters")
+        .max (100,"Preference name must be at most 100 characters"),
+    description:z
+    .string()
+    .trim()
+    .min(2,"Allergen description must be at least 2 characters")
+    .max(100,"Allergen description must be at most 500 characters" )
+})
+.refine((data) => Object.keys(data).length > 0, {
+		message: "At least one field must be provided for update",
+	});
