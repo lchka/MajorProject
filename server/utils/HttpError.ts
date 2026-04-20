48e1a6bf1cd9f743f4d9029429d28369c090498a
Laura Hofmanvoa <N00222003@iadt.ie>
2026-04-20 12:55:51 +0100

    added fallback for 429 error in openweather api key rate limitation

--- server/utils/HttpError.ts
+++ server/utils/HttpError.ts
@@ -12,6 +12,7 @@ export const CREATED_SUCCESS = 201;
 export const SUCCESS_RES=200;
 export const INTERNAL_SERVER_ERROR = 500;
 export const BAD_REQUEST = 400;
+export const SERVICE_UNAVAILABLE = 503;
 export const UNAUTHORISED = 401;
 export const FORBIDDEN = 403;
 export const NOT_FOUND = 404;
